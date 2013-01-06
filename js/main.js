030c2021fc7a2a692b4627550dfe0e43948fc5f3
Alberto Trujillo Gonzalez <altrugon@gmail.com>
2013-01-05 16:11:27 -0800

    Add Spanish content and flip content effect.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+(function($){
+  /*
+   * Flip content
+   */
+  var flip_content = function() {
+    $("h1").click(function(event){
+      $("#languages").toggleClass('flipped');
+      event.preventDefault();
+    });
+  }
+  
+  $(document).ready(function(){
+    flip_content();
+  });
+  
+})(jQuery);
