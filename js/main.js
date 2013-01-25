37d93a9f9b6ef79837f349ec0abe10acb16434b4
Alberto Trujillo Gonzalez <altrugon@gmail.com>
2013-01-24 21:18:57 -0800

    First commit.

---
+++ js/main.js
@@ -0,0 +1,4 @@
+
+(function($){var flip_content=function(){$("nav a").click(function(event){$("#languages").toggleClass('flipped');if($(this).hasClass('spanish')){$('span',this).text('English');$(this).removeClass('spanish').addClass('english');}
+else{$('span',this).text('Español');$(this).removeClass('english').addClass('spanish');}
+event.preventDefault();});};var add_my_links=function(){$('a.twitter').attr('href','https://twitter.com/altrugon');$('a.google-plus').attr('href','https://plus.google.com/u/0/112226124763991192418');$('a.github').attr('href','https://github.com/altrugon');$('a.email').attr('href','mailto:altrugon@gmail.com');};$(document).ready(function(){flip_content();add_my_links();});})(jQuery);
